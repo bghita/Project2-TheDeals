42ea1101ed13429e1f71344b3c5328c00f096995
Becky Ghita <ghita109@gmail.com>
2019-07-02 14:56:42 -0700

    starting project template

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+
+var app = express();
+
+
+var PORT = process.env.PORT || 5080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "deals_db"
+});
