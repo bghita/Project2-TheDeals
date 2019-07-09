4b67762c874f02c9d6b43a31064549e3d0f4cd64
Ryan Gelow <exisphere@gmail.com>
2019-07-09 12:12:31 -0700

    made some more app.js updates, sorry Shantell

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,88 +1,30 @@
-// let couponFeed = require('./../../../controllers/couponController');
+// let grouponCity = "san-francisco";
+// let grouponCat = "Local";
 
-let grouponCity = "san-francisco";
-let grouponCat = "Local";
 
+
+//needs directing... VVVVV
+function categories() {
+    $.ajax({
+        url: "/api/coupons/categories"
+    }).then( response => {
+        return response;
+    })
+}
     
-const categories = [
-    {
-        name: "Local",
-        subcat_name: [
-            "automotive",
-            "beauty-and-spas",
-            "food-and-drink",
-            "health-and-fitness",
-            "home-improvement",
-            "personal-services",
-            "retail",
-            "things-to-do"
-        ],
-        subcat_show: [
-            "Automotive",
-            "Beauty & Spas",
-            "Food & Drink",
-            "Health & Fitness",
-            "Home Improvement",
-            "Personal Services",
-            "Retail",
-            "Things To Do"
-        ]
-    },
-    {
-        name: "Goods",
-        subcat_name: [
-            "auto-and-home-improvement",
-            "baby-kids-and-toys",	
-            "collectibles",
-            "electronics",
-            "entertainment-and-media",	 
-            "for-the-home",
-            "groceries-household-and-pets",	 
-            "health-and-beauty",
-            "jewelry-and-watches",
-            "mens-clothing-shoes-and-accessories",
-            "sports-and-outdoors",
-            "womens-clothing-shoes-and-accessories"
-        ],
-        subcat_show: [
-            "Auto & Home Improvement",
-            "Baby, Kids & Toys",	
-            "Collectibles",
-            "Electronics",
-            "Entertainment & Media",	 
-            "For The Home",
-            "Groceries, Household, & Pets",	 
-            "Health & Beauty",
-            "Jewelry & Watches",
-            "Men's Clothing, Shoes & Accessories",
-            "Sports & Outdoors",
-            "Women's Clothing, Shoes & Accessories"
-        ]
-    },
-    {
-        name: "Travel",
-        subcat_name: [
-            "cruise-travel",
-            "flights-and-transportation",
-            "hotels-and-accommodations",
-            "tour-travel"
-        ],
-        subcat_show: [
-            "Cruise Travel",
-            "Flights & Transportation",
-            "Hotels & Accommodations",
-            "Tour Travel"
-        ]
-    }
-]
-$('#cities').on('click', cities())
+    
+$('#cities-list').on('click', cities())
 
 function cities() {
    $.ajax({
         url: "/api/coupons/cities"
     }).then( response => {
-        return response
+        return response;
     })
 }
+
+//make selection a variable
+
+//
 
 //  You should be making API requests to your routes for data here.
