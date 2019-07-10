026d13cddcd1a3953b597c3b403cbd5e2488900e
Ryan Gelow <exisphere@gmail.com>
2019-07-10 12:32:57 -0700

    added city selector for both sign up and main pages

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,17 @@ const api = require('./api');
 router.use(express.static(path.join(__dirname, "../public")))
 router.use('/api', api);
 
+router.route('/signup')
+    .get(function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/signup.html"));
+    })
+
+
+// router.route('/api/user/:userId/coupon/:couponId')
+//     .get()
+//     .delete()
+
 module.exports = router;
+
+// /api/user/:userId/coupon/:couponId
+
