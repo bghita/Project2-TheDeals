f64c99a9d2dd0a3376cd1d02233ea7eda198cdc1
Shantell Davis <shantell87@gmail.com>
2019-07-11 14:44:08 -0700

    updated location button on index.html and added function to index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,4 +1,25 @@
-
+//location dropdown function
+let dropdown = function() {
+    $.ajax({
+        url: "/api/coupons/cities",
+    }).then( response => {
+        // console.log(response);
+        response.forEach( city => $('#city-selector').append($('<option>').text(city)))
+    })
+};
+// $('#locationBtn').on("click", function(e){
+//     const status = $(this).attr("data-toggle")
+//     console.log(status)
+//     if (status === "off"){
+//         $(this).attr("data-toggle", "on")
+//         $("#city-selector").show(); 
+//     }
+//     else{
+//         $(this).attr("data-toggle", "off")
+//         $("#city-selector").hide(); 
+//     }
+// })
+$('#city-selector').change(dropdown());
 
 
 //login page functions
